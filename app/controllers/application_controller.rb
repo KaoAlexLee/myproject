2ce825d86efd8a7f288d7859eb0bdaf86dd53d71
KaoAlexLee <alex@fi.com.tw>
2011-03-12 16:58:06 +0800

    AddI18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+	protect_from_forgery
+
+	before_filter :set_locale
+               
+	def set_locale
+		# 可以將 ["en", "zh-TW"] 設定為 VALID_LANG 放到 config/environment.rb 中
+		if params[:locale] && ["en", "zh-TW"].include?( params[:locale] )
+			session[:locale] = params[:locale]
+		end
+
+		I18n.locale = session[:locale] || I18n.default_locale
+	end
 end
